3cd1c35a01ae06e58213dbba12eb96079cf44dfc
musabgelisgen <mgelisgen97@gmail.com>
2020-04-26 19:20:12 +0300

    any number of files are accepted now

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import werkzeug
 from subprocess import call
+import os, shutil
 
 
 app = Flask(__name__)
@@ -16,17 +17,33 @@ def read_output():
         result = []
         lis = [line.split() for line in f]
         for i, x in enumerate(lis):
-            result.append(x[1:10]) # these are from confidence to eye gaze angle
+            result.append(x[1:10])  # these are from confidence to eye gaze angle
 
         return jsonify(faces=result)
 
 
 @app.route('/predict', methods=['POST'])
 def get_prediction():
-    read_and_save_file('file1')
-    read_and_save_file('file2')
+    cleanse_uploaded_directory()
+
+    for key in request.files:
+        read_and_save_file(key)
+
     execute_openface_script()
     return read_output()
+
+
+def cleanse_uploaded_directory():
+    folder = '../bin/uploaded'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 def read_and_save_file(file_key):
@@ -37,4 +54,4 @@ def read_and_save_file(file_key):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='80')
+    app.run(debug=True, host='0.0.0.0', port='8000')
